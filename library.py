84c0267e06be4c5d6c077cbce35e38b0838e9e78
Nikhar Ramlakhan <31480711+nikhar-ramlakhan@users.noreply.github.com>
2025-05-11 16:07:44 -0700

    added new functions from chpt9

--- library.py
+++ library.py
@@ -595,6 +595,29 @@ def find_random_state(
 
     return rs_value, Var
 
+def dataset_setup(original_table, label_column_name:str, the_transformer, rs, ts=.2):
+    #your code below
+    labels = original_table[label_column_name].to_list()
+    features = original_table.drop(columns=label_column_name)
+
+    x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=ts, shuffle=True, random_state=rs, stratify=labels)
+
+    x_train_transformed = the_transformer.fit_transform(x_train, y_train)
+    x_test_transformed = the_transformer.transform(x_test)
+
+    x_train_numpy = x_train_transformed.to_numpy()
+    x_test_numpy = x_test_transformed.to_numpy()
+    y_train_numpy = np.array(y_train)
+    y_test_numpy = np.array(y_test)
+
+    return x_train_numpy, x_test_numpy, y_train_numpy, y_test_numpy
+
+def titanic_setup(titanic_table, transformer=titanic_transformer, rs=titanic_variance_based_split, ts=.2):
+  return dataset_setup(titanic_table, 'Survived', transformer, rs, ts)
+
+def customer_setup(customer_table, transformer=customer_transformer, rs=customer_variance_based_split, ts=.2):
+  return dataset_setup(customer_table, 'Rating', transformer, rs, ts)
+
 titanic_transformer_v4 = Pipeline(steps=[
     ('gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
     ('class', CustomMappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
